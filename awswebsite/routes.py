90b20f275a6db4b2a4a641e7cd99455eb2e342ea
JaredDyreson <jared.dyreson@gmail.com>
2019-12-24 00:47:01 -0800

    Automated backup

--- awswebsite/routes.py
+++ awswebsite/routes.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, flash, redirect, request, session, send_file, send_from_directory
 from awswebsite import app
+from awswebsite.forms import PostForm
 from awswebsite.models import User, Post
 
 @app.route("/")
@@ -11,3 +12,12 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title="About")
+
+@app.route("/post/new", methods=['GET', 'POST'])
+def new_post():
+	form = PostForm()
+	if form.validate_on_submit():
+		flash("Your post has been successfully created!", 'success')
+		return redirect(url_for('home'))
+	return render_template('create_post.html', title='New Post', form=form)
+
