7ec1b45493ac168348890017332486a6f8035287
JaredDyreson <jared.dyreson@gmail.com>
2020-07-06 14:15:00 -0700

    working on blog section

---
+++ personalwebsite/BlogStructure.py
@@ -0,0 +1,100 @@
+import pathlib
+import os
+import pprint
+import re
+from termcolor import colored
+
+class BlogHierarchy():
+    def __init__(self, path: pathlib.Path):
+        if not(isinstance(path, pathlib.Path) and
+               path.is_dir()):
+            raise ValueError
+        self.base = path.absolute()
+        self.structure = self.generate_structure()
+        self.topics = self.get_topics()
+
+    def generate_structure(self):
+        structure = {'': {}}
+        for dirpath, dirnames, filenames in os.walk(path):
+            state = structure
+            dirpath = dirpath[len(path.name):]
+            for subdir in dirpath.split(os.sep):
+                based = state
+                state = state[subdir]
+            if(dirnames):
+                for dn in dirnames:
+                    state[dn] = {}
+            else:
+                based[subdir] = filenames
+        return structure
+    
+    def get_topics(self):
+        return list(self.get_topic_structure().keys()) if self.structure is not None else []
+
+    def get_topic_structure(self):
+        return self.structure['']
+
+    def get_subtopic_structure(self):
+        s = {}
+        for topic in self.topics:
+            s[topic] = self.get_topic_structure()[topic]
+        return s
+
+    def search(self, path: tuple):
+        if not(isinstance(path, tuple)):
+            raise ValueError
+
+        root, subject, filename = path
+
+        if not(isinstance(root, str) and
+               isinstance(subject, str) and
+               isinstance(filename, str)):
+               raise ValueError
+
+        path_to_file = pathlib.Path(os.path.join(self.base, root, subject, filename))
+
+        if not(path_to_file.exists()):
+                 raise FileNotFoundError
+
+        return path_to_file
+
+    def grep_blog_contents(self, path: pathlib.Path, regex: str):
+        if not(isinstance(path, pathlib.Path) and
+               isinstance(regex, str)):
+            raise ValueError
+
+        contents = [line for line in path.open()]
+        reg = re.compile(regex)
+        relative_path = '/'.join(path.parts[len(self.base.parts):]) 
+        for x, line in enumerate(contents):
+            match = reg.search(line)
+            if(match):
+                start, end = match.span()
+                match_format = colored(line[start:end], 'red')
+                index_format = colored(f'{relative_path}:{x}', 'green')
+                print(f'{index_format} {match_format}{line[end:]}')
+
+
+structure = {
+    "blog": {
+        "scripting": {
+            "bash": ["hello.md"],
+            "python": ["input.md"]
+        },
+        "notes": {
+            "math": ["slope.md"],
+            "biology": ["cells.md"]
+        }
+    }
+}
+
+pp = pprint.PrettyPrinter(indent=4)
+
+path = pathlib.Path("./blog")
+
+H = BlogHierarchy(path)
+
+example = H.search(("scripting", "python", "input.md"))
+H.grep_blog_contents(example, "^\#\s+(?P<title>.*)")
+out = H.get_subtopic_structure()
+print(example.stat())
