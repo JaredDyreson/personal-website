0a936fb81c2f34c55b2a3a1463e949a26c6cb415
JaredDyreson <jared.dyreson@gmail.com>
2020-07-16 13:59:16 -0600

    where can I write to????

--- personalwebsite/__init__.py
+++ personalwebsite/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 import pathlib
 
 
-UPLOAD_FOLDER = pathlib.Path('/tmp/upload_folder')
+UPLOAD_FOLDER = pathlib.Path('/home/jared/storage')
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
